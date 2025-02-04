91a81975c7bd7de3e778ca6a0f65d88492c5dfcc
Marcos <marcosmartostuvi@gmail.com>
2025-02-04 20:20:19 -0300

    creacion de formulario y enrutado

--- micelium/src/App.jsx
+++ micelium/src/App.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/navbar/Navbar";
+import Footer from "./components/layout/footer/Footer";
+
 function App() {
-  return <h1 className="text-4xl font-bold text-blue-500">Hola soy APP.jsx</h1>;
+  return (
+    <div className="w-full h-screen bg-slate-100">
+      <div className="pt-24">
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Navbar />}>
+              <Route element={<Footer />}>
+                {/* Ruta Principal */}
+                <Route path="/" element={<h1>Home</h1>} />
+
+                {/* Rutas personalizadas */}
+                <Route path="/login" element={<h1>Login</h1>} />
+              </Route>
+            </Route>
+
+            {/* Ruta por defecto */}
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
 
 export default App;
